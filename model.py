6d29eb4ad490ee00920d70ed29b3cfb22f7978cc
lvllvl <chrisidxloop@gmail.com>
2023-11-07 20:27:55 -0500

    updating model.py with u-net layers

--- model.py
+++ model.py
@@ -11,3 +11,27 @@ from config import (
     MODEL_SAVE_NAME,
     SAVE_FREQUENCY,
 )
+import torch.nn as nn
+
+class UNet( nn.Module ):
+
+    def __init__( self ):
+        super( UNet, self ).__init__()
+        # ex of 1st conv layer (Unet)
+        self.conv1 = nn.Conv2d( 3, 64, kernel_size=3, stride=1, padding=1 )
+        self.relu1 = nn.ReLU( inplace=True )
+        self.conv2 = nn.Conv2d( 64, 64, kernel_size=3, stride=1, padding=1 )
+        self.relu2 = nn.ReLU( inplace=True )
+        self.maxpool1 = nn.MaxPool2d( kernel_size=2, stride=2 )
+
+    def forward( self, x ):
+        # Layers to input
+        x = self.conv1( x )
+        x = self.relu1( x )
+        x = self.conv2( x )
+        x = self.relu2( x )
+        x = self.maxpool1( x )
+        #  more operations 
+        
+        return x 
+
